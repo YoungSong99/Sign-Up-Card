3f855f00ac363df353ff1b71a5032a9f418fbeec
youngsong99 <hyangyoungs@gmail.com>
2024-05-25 10:54:02 -0500

    feat: signup logic

--- signup.js
+++ signup.js
@@ -22,46 +22,162 @@ document.addEventListener("DOMContentLoaded", function () {
     window.handlePhoneInput = handlePhoneInput;
 
     let isAuthStarted = false;
+    let time = 10;
+    let timer;
 
-    const sendToken = () => {
+    const generateToken = () => {
+        let token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
+        document.getElementById("token").innerText = token;
+        document.getElementById("send_token").disabled = true;
+    }
+
+    window.generateToken = generateToken;
+
+    const refreshTokenStatus = () => {
+        isAuthStarted = false;
+        clearInterval(timer)
+        document.getElementById("token").innerText = "000000";
+        document.getElementById("complete_token").disabled = true;
+        document.getElementById("send_token").disabled = false;
+    }
+
+    window.refreshTokenStatus = refreshTokenStatus;
 
 
-        if (isAuthStarted === false){
+    const tokenTimer = () => {
+
+        if (isAuthStarted === false) {
             isAuthStarted = true;
-            const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
-            document.getElementById("token").innerText = token;
-            document.getElementById("send_token").disabled = true;
 
-            let time = 3;
+            generateToken()
 
-            let timer = setInterval(function () {
-                if(time >= 0){
+            timer = setInterval(function () {
+                if (time >= 0) {
 
                     // button active
                     document.getElementById("complete_token").disabled = false;
 
                     // time update
-                    let min = String(Math.floor(time/60)).padStart(2,"0")
-                    let sec = String(time%60).padStart(2,"0")
+                    let min = String(Math.floor(time / 60)).padStart(2, "0")
+                    let sec = String(time % 60).padStart(2, "0")
 
                     document.getElementById("min").innerText = min;
                     document.getElementById("sec").innerText = sec;
 
                     time--
                 } else {
-                    isAuthStarted = false;
-                    clearInterval(timer)
-                    document.getElementById("token").innerText = "000000";
-                    document.getElementById("complete_token").disabled = true;
-                    document.getElementById("send_token").disabled = false;
+                    refreshTokenStatus()
                 }
             }, 1000)
         }
-
-
-
-
     };
-    window.sendToken = sendToken;
+
+    window.tokenTimer = tokenTimer;
+
+
+    let formFields = {
+        "email": false,
+        "name": false,
+        "password": false,
+        "password-conf": false,
+        "phone": false,
+        "region_dropdown": false,
+        "gender": false,
+        "agree": false
+    }
+
+    const tokenConfirmed = () => {
+        alert("Verification is completed");
+        refreshTokenStatus()
+        formFields["phone"] = true;
+    }
+
+    window.tokenConfirmed = tokenConfirmed;
+
+    const isFilled = (id) => {
+
+        let input_value = document.getElementById(id).value
+
+
+        if (id === "Male" || id === "Female" || id === "Prefer not to say" || id === "Other") {
+            if (input_value) {
+                formFields["gender"] = true;
+            }
+        } else {
+            if (input_value && input_value.trim() !== "") {
+                formFields[id] = true;
+            } else {
+                formFields[id] = false;
+            }
+        }
+
+        document.getElementById("submit_btn").disabled = Object.values(formFields).includes(false);
+    }
+
+    window.isFilled = isFilled;
+
+
+    const submitVerification = () => {
+
+        isValid = true;
+
+        let email = document.getElementById("email").value
+        let name = document.getElementById("name").value
+        let password = document.getElementById("password").value
+        let password_confirm = document.getElementById("password-conf").value
+        let region = document.getElementById("region_dropdown").value
+
+        let Male = document.getElementById("Male").checked
+        let Female = document.getElementById("Female").checked
+        let Not = document.getElementById("Prefer not to say").checked
+        let Other = document.getElementById("Other").checked
+
+
+
+        if (!email.includes("@")){
+            document.getElementById("email_error").style.display = 'block'
+            isValid = false;
+        } else {
+            document.getElementById("email_error").style.display = 'none'
+        }
+
+        if (name === ""){
+            document.getElementById("name_error").style.display = 'block'
+            isValid = false;
+        } else {
+            document.getElementById("name_error").style.display = 'none'
+        }
+
+
+        if (password !== password_confirm){
+            document.getElementById("password_error").style.display = 'block'
+            isValid = false;
+        } else {
+            document.getElementById("password_error").style.display = 'none'
+        }
+
+
+        if(region === "" || !region){
+            document.getElementById("region_error").style.display = 'block'
+            isValid = false;
+        } else {
+            document.getElementById("region_error").style.display = 'none'
+        }
+
+        if( Male||Female||Not||Other ) {
+            document.getElementById("gender_error").style.display = 'none'
+        } else {
+            document.getElementById("gender_error").style.display = 'block'
+            isValid = false;
+        }
+
+        console.log(isValid)
+
+        if (isValid) {
+            alert("Your registration has been successfully completed.")
+        }
+
+    }
+    window.submitVerification = submitVerification;
 
 });
